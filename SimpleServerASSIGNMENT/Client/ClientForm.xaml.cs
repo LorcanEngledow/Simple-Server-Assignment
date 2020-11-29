6a120333c01bc3b61ca8473192e20b4ce3486746
LEngledow <57404195+LEngledow@users.noreply.github.com>
2020-11-29 06:17:01 +0000

    Fully Ported Client Project Fully added Port from net core to net framework complete

--- SimpleServerASSIGNMENT/Client/ClientForm.xaml.cs
+++ SimpleServerASSIGNMENT/Client/ClientForm.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,7 +8,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Client
@@ -18,11 +15,29 @@ namespace Client
     /// <summary>
     /// Interaction logic for ClientForm.xaml
     /// </summary>
-    public partial class ClientForm : UserControl
+    public partial class ClientForm : Window
     {
-        public ClientForm()
+        Client client;
+
+        public ClientForm(Client pClient)
         {
             InitializeComponent();
+
+            client = pClient;
+        }
+
+        public void UpdateChatWindow(string message)
+        {
+            MessageWindow.Dispatcher.Invoke(() =>
+            {
+                MessageWindow.Text += message + Environment.NewLine;
+                MessageWindow.ScrollToEnd();
+            });
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            client.SendMessage();
         }
     }
 }
---
+++ SimpleServerASSIGNMENT/Client/ClientMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Client
+{
+    class ClientMain
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            if (client.Connect("127.0.0.1", 4444))
+            {
+                Console.WriteLine("Successfully connected");
+                client.Run();
+            }
+            else
+                Console.WriteLine("Connection Failed");
+        }
+    }
+}
